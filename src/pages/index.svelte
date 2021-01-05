f49e238293f628907a0a993e26fad976b8a49592
Arief Buddies <54927162+ariefbuddies@users.noreply.github.com>
2021-01-05 17:06:47 +0700

    Update index.svelte

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="starter-template text-center py-5 px-3">
-	<h1>Svelte Garuda template</h1>
+	<h1>Svelte GarudaBRA</h1>
 	<p class="lead">
 		Use this template as a way to quickly start any new svelte project.
 	</p>
